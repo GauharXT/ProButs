a53cc436944a6e4dc97b2f26fe3c63409c6d732b
danielabdullaev <danielabdullaev18@gmail.com>
2025-04-29 17:22:11 +0600

    commit

--- store/admin.py
+++ store/admin.py
@@ -1,19 +1,16 @@
 from django.contrib import admin
 from .models import Product, Category
-from django.contrib import admin
-from .models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('brand', 'gender')
-    search_fields = ('brand', 'gender')
-    list_filter = ('brand', 'gender')
+    list_display = ( 'brand', 'material',)
+    search_fields = ('brand', 'material',)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'brand', 'category', 'price', 'size',
+        'name', 'brand', 'category', 'price', 'size',
         'color', 'material', 'gender', 'is_available', 'created_at'
     )
-    search_fields = ('brand', 'color', 'material')
+    search_fields = ('name', 'brand', 'color', 'material')
     list_filter = ('brand', 'category', 'size', 'color', 'gender', 'is_available', 'created_at')
--- store/models.py
+++ store/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.utils.text import slugify
 
 class Category(models.Model):
-    BRAND_CHOICES = [
+    GENDER_CHOICES = [
         ('adidas', 'Adidas'),
         ('demix', 'Demix'),
         ('emporio armani', 'Emporio Armani'),
@@ -15,50 +14,47 @@ class Category(models.Model):
         ('puma','Puma'),
         ('outventure','Outventure'),
         ('speedo', 'Speedo'),
-        ('under armour','Under Armour')
+        ('ander armour','Under Armour')
     ]
-
-    GENDER_CHOICES = [
-        ('male', 'Male'),
-        ('female', 'Female'),
-        ('unisex', 'Unisex'),
-    ]
-
-    brand = models.CharField(max_length=100, choices=BRAND_CHOICES)
-    slug = models.SlugField(unique=True, blank=True)
-    description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='category_images/', blank=True, null=True)
-    gender = models.CharField(max_length=100, choices=GENDER_CHOICES)
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.brand)
-        super().save(*args, **kwargs)
+    brand = models.CharField(max_length=100, choices=GENDER_CHOICES)
+    material = models.CharField(max_length=100, null=True, blank=True,)
 
     def __str__(self):
         return self.brand
-
-
 class Product(models.Model):
     GENDER_CHOICES = [
         ('male', 'Male'),
         ('female', 'Female'),
         ('unisex', 'Unisex'),
     ]
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
 
-    brand = models.CharField(max_length=100)
+    # GENDER_BRAND = [
+    #     ('adidas', 'Adidas'),
+    #     ('demix', 'Demix'),
+    #     ('emporio armani', 'Emporio Armani'),
+    #     ('hugo boss', 'Hugo Boss'),
+    #     ('jack wolfskin', 'Jack Wolfskin'),
+    #     ('joss', 'Joss'),
+    #     ('kelme', 'Kelme'),
+    #     ('molten', 'Molten'),
+    #     ('nike', 'Nike'),
+    #     ('puma', 'Puma'),
+    #     ('outventure', 'Outventure'),
+    #     ('speedo', 'Speedo'),
+    #     ('ander armour', 'Under Armour')
+    # ]
+
+    name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     size = models.CharField(max_length=10)
+    brand = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     color = models.CharField(max_length=50)
     material = models.CharField(max_length=100, null=True, blank=True)
-    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     image = models.ImageField(upload_to='product_images/')
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.brand}"
+        return self.name
