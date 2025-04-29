19e68310c345a9a0a8eab8770e7b5f7721f7b4a6
fedropoll <arstanbekovasil10@gmail.com>
2025-04-29 15:12:08 +0600

    регистер коштум

---
+++ register/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={'input_type': 'password'}
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone', 'password', 'password2']
+        extra_kwargs = {
+            'email': {'required': True},
+            'username': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        if User.objects.filter(email=attrs['email']).exists():
+            raise serializers.ValidationError({"email": "Email already exists"})
+
+        if 'phone' in attrs and User.objects.filter(phone=attrs['phone']).exists():
+            raise serializers.ValidationError({"phone": "Phone already exists"})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            phone=validated_data.get('phone', None)
+        )
+        return user
